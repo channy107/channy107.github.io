61658bd851d966a40d14cf04edf3fea80d1f8458
channy107 <hunchanlim@HUNCHANui-MacBookAir.local>
2023-08-07 18:56:17 +0900

    댓글 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230807.185143';
+const cacheName = 'chirpy-20230807.185556';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
