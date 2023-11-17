2f4739c2952347ee0b2de1447855a96f652c156a
channy107 <hunchanlim@HUNCHANui-MacBookAir.local>
2023-11-17 14:50:27 +0900

    실행 컨텍스트 포스팅

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231102.190411';
+const cacheName = 'chirpy-20231117.145009';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
