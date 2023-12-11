4973afd3aa627884ab4a4279c92d3800ca8eae96
channy107 <hunchanlim@HUNCHANui-MacBookAir.local>
2023-12-11 09:59:08 +0900

    this

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231122.204931';
+const cacheName = 'chirpy-20231211.095822';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
